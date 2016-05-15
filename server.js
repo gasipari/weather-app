5f44b843044d44f78e739734213990a77807a9da
gasipari <riamarius@gmail.com>
2016-05-15 00:01:13 -0400

    Prepare repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var express = require('express');
 
 // create the app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
+// redirect https request to http
+// to fix openWeatherMap http only issue
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
 })
