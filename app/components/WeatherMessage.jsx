abe05578bbb5db112d1b62942c21de5ab3664a9b
gasipari <riamarius@gmail.com>
2016-05-16 12:20:50 -0400

    Install foundation framework

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({location,temp}) => {
   return (
-    <h3>It is {temp} in {location} new</h3>
+    <h3>It is {temp} in {location}</h3>
   );
 }
 
