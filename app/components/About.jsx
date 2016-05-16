b263013fe20bf2be9fb867f67b1c179a1e9f6962
gasipari <riamarius@gmail.com>
2016-05-16 16:33:10 -0400

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About page!</p>
-    </div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React. It is a demonstration of react/redux features and foundation framework</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://github.com/facebook/react">React.js</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org/">Open Weather Map</a> - Open Weather Map API was used to search for weather data by city name.
+        </li>
+      </ul>
+  </div>
   );
 }
 
