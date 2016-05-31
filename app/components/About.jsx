13b9009b94ff15d4d0e4e1e6529e39a751f03764
gasipari <riamarius@gmail.com>
2016-05-31 12:10:07 -0400

    Update About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,18 +4,17 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">About</h1>
-      <p>This is a weather application build on React. It is a demonstration of react/redux features and foundation framework</p>
-      <p>
-        Here are some of the tools I used:
-      </p>
+      <p>This is a weather application build using:</p>
       <ul>
         <li>
-          <a href="https://github.com/facebook/react">React.js</a> - This was the JavaScript framework used.
+          <a href="https://github.com/facebook/react">React.js</a> - A JavaScript library for building user interfaces.
         </li>
         <li>
-          <a href="http://openweathermap.org/">Open Weather Map</a> - Open Weather Map API was used to search for weather data by city name.
+          <a href="http://openweathermap.org/">Open Weather Map</a> - Open Weather Map API used to search for weather data by city name.
         </li>
       </ul>
+      <p>For more details, please visit the <a href="https://github.com/gasipari/weather-app"
+        target="_blank">repository</a> on github.</p>
   </div>
   );
 }
