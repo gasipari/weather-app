b54861977a9f2f4a4a14c08ba7b95700f0e4cafc
gasipari <riamarius@gmail.com>
2016-05-31 20:32:06 -0400

    Add Google map

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,13 +4,19 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">About</h1>
-      <p>This is a weather application build using:</p>
+      <p>This application was build using:</p>
       <ul>
         <li>
-          <a href="https://github.com/facebook/react">React.js</a> - A JavaScript library for building user interfaces.
+          <a href="https://github.com/facebook/react" target="_blank">React.js</a> - A JavaScript library for building user interfaces.
         </li>
         <li>
-          <a href="http://openweathermap.org/">Open Weather Map</a> - Open Weather Map API used to search for weather data by city name.
+          <a href="http://foundation.zurb.com/" target="_blank">Zurb Foundation</a> - The most advanced responsive front-end framework in the world.
+        </li>
+        <li>
+          <a href="http://openweathermap.org/" target="_blank">Open Weather Map</a> - Open Weather Map API used to search for weather data by city name.
+        </li>
+        <li>
+          <a href="https://developers.google.com/maps/documentation/javascript/" target="_blank">Google Maps APIs</a> - Google Map API used to retrieve geographic coordinates data by city name.
         </li>
       </ul>
       <p>For more details, please visit the <a href="https://github.com/gasipari/weather-app"
